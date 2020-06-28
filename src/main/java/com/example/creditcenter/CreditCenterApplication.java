6ae2781d476b15041a7ff8df480015b3b60655e6
FangYuan__Liu@126.com <FangYuan__Liu@126.com>
2020-06-28 22:12:15 +0800

    init credit-center

---
+++ src/main/java/com/example/creditcenter/CreditCenterApplication.java
@@ -0,0 +1,13 @@
+package com.example.creditcenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CreditCenterApplication.class, args);
+    }
+
+}
