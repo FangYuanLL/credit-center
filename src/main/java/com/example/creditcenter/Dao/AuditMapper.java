cb2976ac8a11015c45b76b477381ff857103c117
FangYuan__Liu@126.com <FangYuan__Liu@126.com>
2020-07-05 21:15:09 +0800

    加@Repository 解决Property 'sqlSessionFactory' or 'sqlSessionTemplate' are required问题

--- src/main/java/com/example/creditcenter/Dao/AuditMapper.java
+++ src/main/java/com/example/creditcenter/Dao/AuditMapper.java
@@ -1,7 +1,11 @@
 package com.example.creditcenter.Dao;
 
 import com.example.creditcenter.Model.Audit;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AuditMapper {
+    //Audit selectAuditById(Integer id);
+    @ChoseDataSource("master")
     Audit selectAuditById(Integer id);
 }
